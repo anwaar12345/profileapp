533d737d1982f4264eac8ead2c6d01eabd670052
AnwaarAhmedshah <fa15be0011@maju.edu.pk>
2020-01-02 15:16:16 +0500

    2nd last majoor change n style

--- admin/header.php
+++ admin/header.php
@@ -41,8 +41,8 @@ $row = mysqli_fetch_array($q);
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="http://cijulenlinea.ucr.ac.cr/dev-users/">
-                    <img src="http://placehold.it/200x50&text=LOGO" alt="LOGO">
+                <a class="navbar-brand" href="dashboard.php" style="height:100px;">
+                    <img src="../upload/<?php echo $row['image'];?>" alt="LOGO" height="80px" width="100px">
                 </a>
             </div>
             <!-- Top Menu Items -->
@@ -61,8 +61,8 @@ $row = mysqli_fetch_array($q);
                 </li>
             </ul>
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav side-nav">
+            <div class="collapse navbar-collapse navbar-ex1-collapse" >
+                <ul class="nav navbar-nav side-nav" style="margin-top:40px;">
                     <li>
                         <a href="#" data-toggle="collapse" data-target="#submenu-1"><i class="fa fa-fw fa-search"></i> Users <i class="fa fa-fw fa-angle-down pull-right"></i></a>
                         <ul id="submenu-1" class="collapse">
@@ -71,23 +71,7 @@ $row = mysqli_fetch_array($q);
                             <li><a href="reporting.php"><i class="fa fa-angle-double-right"></i> Reporting</a></li>
                         </ul>
                     </li>
-                    <li>
-                        <a href="#" data-toggle="collapse" data-target="#submenu-2"><i class="fa fa-fw fa-star"></i>  MENU 2 <i class="fa fa-fw fa-angle-down pull-right"></i></a>
-                        <ul id="submenu-2" class="collapse">
-                            <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 2.1</a></li>
-                            <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 2.2</a></li>
-                            <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 2.3</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="investigaciones/favoritas"><i class="fa fa-fw fa-user-plus"></i>  MENU 3</a>
-                    </li>
-                    <li>
-                        <a href="sugerencias"><i class="fa fa-fw fa-paper-plane-o"></i> MENU 4</a>
-                    </li>
-                    <li>
-                        <a href="faq"><i class="fa fa-fw fa fa-question-circle"></i> MENU 5</a>
-                    </li>
+                    
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
